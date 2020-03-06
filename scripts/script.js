e1906801ac1454ba981f6d7c34c500614ef1e9fa
Sean Clayton <sean.clayto@gmail.com>
2020-03-06 13:52:57 -0800

    logic for creating grid

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function makeGrid() {
+    let grid = [[], [], [], []];
+    let images = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            let selection = Math.floor(Math.random() * images.length);
+            grid[i][j] = images[selection];
+            console.log(grid[i][j]);
+            images.splice(selection, 1);
+        }
+    }
+    console.log(grid);
+}
+makeGrid();
