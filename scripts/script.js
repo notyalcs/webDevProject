98d9ec8128035b849fa57c2b7d466512a391219c
Joban <joban.nijjer145@gmail.com>
2020-03-19 10:26:10 -0700

    Added Placeholders

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 var wordBank = ["hello", "goodbye", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
 let randomWord = "";
+let wordAnswer = "";
+let maxWrong = 6;
+let wordLength = 0;
+let status = null;
+
 function createButtons() {
     let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
     for (let i = 0; i < alphabet.length; i++) {
@@ -9,9 +14,18 @@ function createButtons() {
         userKeyboard.appendChild(button);
     }
 }
-function chooseWord(){
+
+function chooseWord() {
     randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
     console.log(randomWord);
+    document.getElementById("maxLetters").innerHTML = maxWrong;
+    wordLength = randomWord.length;
+}
+function wordGuess(){
+    for(int i = 0; i < wordLength; i++){
+        
+    }
+    document.getElementById("guessedWord").innerHTML = status
 }
 createButtons();
 chooseWord();
